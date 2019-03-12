b27c29428e02127ca6d4a3b9219c2a5b5a0cd91d
Michael (Minghao) Li <minghao.li1@ucalgary.ca>
2019-03-11 18:27:48 -0600

    process test

--- data_loader/scripts/2-split_genotypes.py
+++ data_loader/scripts/2-split_genotypes.py
@@ -10,25 +10,19 @@ raw_genotype: str = sys.argv[1]
 processed_dir: str = sys.argv[2]
 
 chr_loc: pd.DataFrame = pd.read_csv(raw_genotype, usecols = ["CHR", "LOC"])
+snp: pd.DataFrame = chr_loc.CHR.astype(str) + '_' + chr_loc.LOC.astype(str)
+del chr_loc
 
-dir: str
-for dir in os.listdir(processed_dir):
-    id_list: str = os.path.join(
-        processed_dir, dir, "id_list_" + dir + ".csv"
-    )
-    iids: pd.Series = pd.read_csv(id_list, squeeze = True)
+pt: str = os.path.basename(processed_dir)
+id_list: str = os.path.join(processed_dir, "id_list_" + pt + ".csv")
+iids: pd.Series = pd.read_csv(id_list, squeeze = True)
 
-    pt_iid_gt: pd.DataFrame = pd.read_csv(
-        raw_genotype, usecols = iids.astype(str)
-    )
-    pt_iid_gt.columns: pd.Index = pt_iid_gt.columns.astype(int)
+pt_iid_gt: pd.DataFrame = pd.read_csv(raw_genotype, usecols = iids.astype(str))
+pt_iid_gt.columns: pd.Index = pt_iid_gt.columns.astype(int)
 
-    output: pd.DataFrame = pd.concat(
-        [chr_loc, pt_iid_gt.sort_index(axis = 1)], axis = 1
-    )
-    output.to_csv(
-        os.path.join(processed_dir, dir, "lipids_genotype_" + dir + ".csv"),
-        index = False,
-        header = True
-    )
+output_path = os.path.join(processed_dir, "lipids_genotype_" + pt + ".csv")
+output: pd = pd.concat(
+    [snp, pt_iid_gt.sort_index(axis = 1)], axis = 1
+).transpose()
+output.to_csv(index = True, index_label = "IID", header = False)
 
