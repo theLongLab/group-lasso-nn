99a908e00d413c7684449574d512c85fe6b7d98b
Michael (Minghao) Li <minghao.li1@ucalgary.ca>
2019-03-13 17:04:26 -0600

    train test appending fix

--- data_loader/scripts/3-train_test_split.py
+++ data_loader/scripts/3-train_test_split.py
@@ -19,9 +19,20 @@ phenotypes: pd.DataFrame = pd.read_csv(phenotype_file, index_col = 0)
 
 os.mkdir(os.path.join(output_root, "train"))
 os.mkdir(os.path.join(output_root, "test"))
+pt: str = os.path.basename(output_root)
 
+chunk_idx: int
+gt_chunk: pd.DataFrame
 for chunk_idx, gt_chunk in enumerate(genotypes):
-    file_mode: str = 'w' if chunk_idx == 0 else 'a'
+    print("\ncurr chunk: {}".format(chunk_idx))
+    file_mode: str
+    header_write: bool
+    if chunk_idx == 0:
+        file_mode = 'w'
+        header_write = True
+    else:
+        file_mode = 'a'
+        header_write = False
 
     gt_test: pd.DataFrame
     pt_train: pd.DataFrame
@@ -29,33 +40,41 @@ for chunk_idx, gt_chunk in enumerate(genotypes):
     gt_train, gt_test, pt_train, pt_test = train_test_split(
         gt_chunk, phenotypes.loc[gt_chunk.index.values], test_size = 0.2
     )
+    print("split done")
 
-    pt: str = os.path.basename(output_root)
     gt_train.to_csv(
         os.path.join(
             output_root, "train", "lipids_genotype_" + pt + "_train.csv"
         ),
+        header = header_write,
         mode = file_mode
     )
+    print("gt train done")
 
     gt_test.to_csv(
         os.path.join(
             output_root, "test", "lipids_genotype_" + pt + "_test.csv"
         ),
+        header = header_write,
         mode = file_mode
     )
+    print("gt test done")
 
     pt_train.to_csv(
         os.path.join(
             output_root, "train", "lipids_phenotype_" + pt + "_train.csv"
         ),
+        header = header_write,
         mode = file_mode
     )
+    print("pt train done")
 
     pt_test.to_csv(
         os.path.join(
             output_root, "test", "lipids_phenotype_" + pt + "_test.csv"
         ),
+        header = header_write,
         mode = file_mode
     )
+    print("pt test done")
 
