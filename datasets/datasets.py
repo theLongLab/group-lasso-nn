77bb0090ab91465cd6ef42366f0d50f41e0cdc19
Michael (Minghao) Li <minghao.li1@ucalgary.ca>
2019-03-13 12:24:06 -0600

    custom dataset implemented

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1,38 +1,53 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from typing import Callable, Tuple
 
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 import torch
 from torch.utils.data import Dataset
 
 
-genotype_file = sys.argv[1]
-phenotype_file = sys.argv[2]
-
-
-
-class LipidGenotypeDataset(Dataset):
+class LipidDataset(Dataset):
     """
-    genotype
+    Lipid dataset class. Assuming genotypes and phenotypes are sorted properly
+    and rows are index matched.
     """
     def __init__(
         self,
-        file_path: str,
-        transform = None
+        genotype_file: str,
+        phenotype_file: str,
+        transforms = None
     ) -> None:
-        pass
+        ohe: OneHotEncoder = OneHotEncoder()  # encoder
+        self.transforms: Callable = torch.from_numpy()
 
-    def __len__(self):
-        return None
+        # One hot encoded input.
+        # - 0 = homozygous ref
+        # - 1 = heterozygous
+        # - 2 = homozygous alt
+        self.genotypes: torch.Tensor = self.transforms(
+            ohe.fit_transform(
+                pd.read_csv(genotype_file).drop("IID", axis = 1)
+            ).todense()
+        )
 
-    def __getitem__(self, idx):
-        return None
+        # Target.
+        self.phenotypes: torch.Tensor = self.transform(
+            pd.read_csv(phenotype_file, usecols = [1]).values
+        )
+
+        self.data_len: int = len(self.phenotypes.index)  # sample size
 
 
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        X: torch.Tensor = self.genotypes[idx]
+        y: torch.Tensor = self.phenotypes[idx]
+        return (X, y)
 
 
-
-
+    def __len__(self) -> int:
+        return self.data_len
 
