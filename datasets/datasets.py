380a082240fd46099c5bb6a422181042064cf8c3
Michael (Minghao) Li <minghao.li1@ucalgary.ca>
2019-03-14 12:14:05 -0600

    chunking implemented in dataset

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -17,41 +17,54 @@ class LipidDataset(Dataset):
     and rows are index matched.
     """
     def __init__(self, root: str, train: bool, transforms = None) -> None:
-        ohe: OneHotEncoder = OneHotEncoder()  # encoder
+        ohe: OneHotEncoder = OneHotEncoder(categories = "auto")  # encoder
         self.transforms: Callable = torch.from_numpy()
 
         phenotype_dir: str = os.path.basename(root)
-        input_file: str
-        target_file: str
+        train_test: str = "test"
         if train:
-            input_file = os.path.join(
-                phenotype_dir, "train",
-                "lipids_genotype_" + phenotype_dir + "_train.csv"
-            )
-            target_file = os.path.join(
-                phenotype_dir, "train",
-                "lipids_phenotype_" + phenotype_dir + "_train.csv"
-            )
+            train_test = "train"
 
-        else:
-            input_file = os.path.join(
-                phenotype_dir, "test",
-                "lipids_genotype_" + phenotype_dir + "_test.csv"
-            )
-            target_file = os.path.join(
-                phenotype_dir, "test",
-                "lipids_phenotype_" + phenotype_dir + "_test.csv"
-            )
+        input_file = os.path.join(
+            phenotype_dir, train_test,
+            "lipids_genotype_" + phenotype_dir + '_' + train_test + ".csv"
+        )
+        target_file = os.path.join(
+            phenotype_dir, train_test,
+            "lipids_phenotype_" + phenotype_dir + '_' + train_test + ".csv"
+        )
 
         # One hot encoded input.
         # - 0 = homozygous ref
         # - 1 = heterozygous
         # - 2 = homozygous alt
-        self.genotypes: torch.Tensor = self.transforms(
-            ohe.fit_transform(
-                pd.read_csv(input_file).drop("IID", axis = 1)
-            ).todense()
-        )
+        self.genotypes: torch.Tensor
+        ohe_dummy_rows: pd.DataFrame
+        num_cols: int
+        genotypes: TextFileReader = pd.read_csv(input_file, chunksize = 500)
+
+        gt_chunk: pd.DataFrame
+        chunk_idx: int
+        for chunk_idx, gt_chunk in enumerate(genotypes):
+            if chunk_idx == 0:
+                num_cols = len(gt_chunk.columns) - 1  # not counting IID col
+                dummy_row: np.ndarray = np.zeros(num_cols, dtype = int)
+                ohe_dummy_rows = pd.DataFrame(
+                    columns = gt_chunk.columns,
+                    data = np.array([dummy_row, dummy_row + 1, dummy_row + 2])
+                )
+
+            gt_chunk = pd.concat(
+                [gt_chunk.drop("IID", axis = 1), ohe_dummy_rows]
+            )
+            gt_chunk_tensor: torch.Tensor = self.transforms(
+                ohe.fit_transform(gt_chunk).todense()[:-3, :]  # exclude dummies
+            )
+
+            try:
+                self.genotypes = torch.cat([self.genotypes, gt_chunk_tensor])
+            except NameError:
+                self.genotypes = gt_chunk_tensor
 
         # Target.
         self.phenotypes: torch.Tensor = self.transform(
