f952e45e85f69e383776c51a5ba6ff3406701a34
Michael (Minghao) Li <minghao.li1@ucalgary.ca>
2019-03-27 17:07:05 -0600

    metric adjustment

--- model/metric.py
+++ model/metric.py
@@ -28,7 +28,9 @@ def adj_rsqr(
     input_feats: int
 ) -> torch.Tensor:
     with torch.no_grad():
-        rsquared: torch.Tensor = rsquared(output, target)
+        ss_res: torch.Tensor = torch.sum((target - output) ** 2)
+        ss_tot: torch.Tensor = torch.sum((target - target.mean()) ** 2)
+        rsquared: torch.Tensor = 1 - ss_res / ss_tot
         adj_rsqr: torch.Tensor = 1 - (1 - rsquared) * (len(target) - 1) \
             / (len(target) - input_feats - 1)
         return adj_rsqr
