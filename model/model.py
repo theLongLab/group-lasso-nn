a8d2d29a3d093bcebfa6db361160863a4034f6b4
Michael (Minghao) Li <minghao.li1@ucalgary.ca>
2019-03-27 18:10:38 -0600

    model update

--- model/model.py
+++ model/model.py
@@ -12,8 +12,12 @@ from base import BaseModel
 class LipidMLP(BaseModel):
     def __init__(self, input_feats: int) -> None:
         super(LipidMLP, self).__init__()
-        self.fc1 = nn.Linear(input_feats, 100)
-        self.fc2 = nn.Linear(100, 1)
+        self.fc1 = nn.Linear(input_feats, 30000)
+        self.fc2 = nn.Linear(30000, 3000)
+        self.fc3 = nn.Linear(3000, 300)
+        self.fc4 = nn.Linear(300, 30)
+        self.fc5 = nn.Linear(30, 3)
+        self.fc6 = nn.Linear(3, 1)
         self.dropout = nn.Dropout(0.2)
 
 
@@ -21,6 +25,14 @@ class LipidMLP(BaseModel):
         x: torch.Tensor = inputs[0]
         x = F.relu(self.fc1(x))  # data is already flat, no need to flatten
         x = self.dropout(x)
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc3(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc4(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc5(x))
+        x = self.dropout(x)
+        x = self.fc6(x)
         return x
 
