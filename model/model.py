742f1924419b1df69d6069b9156592db811306e9
Michael (Minghao) Li <minghao.li1@ucalgary.ca>
2019-04-01 14:20:46 -0600

    better prelu implementation

--- model/model.py
+++ model/model.py
@@ -12,49 +12,48 @@ from base import BaseModel
 class LipidMLP_LR(BaseModel):
     def __init__(self, input_feats: int) -> None:
         super(LipidMLP_LR, self).__init__()
-        self.fc1 = nn.Linear(input_feats, 1)
+        self.output = nn.Linear(input_feats, 1)
 
 
     def forward(self, *inputs: torch.Tensor) -> torch.Tensor:
         x: torch.Tensor = inputs[0]
-        x = self.fc1(x)
-        return x
+        return self.output(x)
 
 
 class LipidMLP_Shallow(BaseModel):
     def __init__(self, input_feats: int) -> None:
         super(LipidMLP_Shallow, self).__init__()
-        self.fc1 = nn.Linear(input_feats, 100)
-        self.fc2 = nn.Linear(100, 1)
+        self.dense1 = nn.Linear(input_feats, 100)
+        self.output = nn.Linear(100, 1)
+        self.prelu = nn.PReLU()
         self.dropout = nn.Dropout(0.2)
 
 
     def forward(self, *inputs: torch.Tensor) -> torch.Tensor:
         x: torch.Tensor = inputs[0]
-        x = F.relu(self.fc1(x))  # data is already flat, no need to flatten
+        x = self.prelu(self.fc1(x))  # data is already flat, no need to flatten
         x = self.dropout(x)
-        x = self.fc2(x)
-        return x
+        return self.output(x)
 
 
 class LipidMLP_Deep(BaseModel):
     def __init__(self, input_feats: int) -> None:
         super(LipidMLP_Deep, self).__init__()
-        self.fc1 = nn.Linear(input_feats, 1000)
-        self.fc2 = nn.Linear(1000, 100)
-        self.fc3 = nn.Linear(100, 10)
-        self.fc4 = nn.Linear(10, 1)
+        self.dense1 = nn.Linear(input_feats, 1000)
+        self.dense2 = nn.Linear(1000, 100)
+        self.dense3 = nn.Linear(100, 10)
+        self.output = nn.Linear(10, 1)
+        self.prelu = nn.PReLU()
         self.dropout = nn.Dropout(0.2)
 
 
     def forward(self, *inputs: torch.Tensor) -> torch.Tensor:
         x: torch.Tensor = inputs[0]
-        x = F.prelu(self.fc1(x))  # data is already flat, no need to flatten
+        x = self.prelu(self.fc1(x))  # data is already flat, no need to flatten
         x = self.dropout(x)
-        x = F.prelu(self.fc2(x))
+        x = self.prelu(self.fc2(x))
         x = self.dropout(x)
-        x = F.prelu(self.fc3(x))
+        x = self.prelu(self.fc3(x))
         x = self.dropout(x)
-        x = self.fc4(x)
-        return x
+        return self.output(x)
 
